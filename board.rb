1d2f55ca3a74f94f2e725a7b21e60f291a8a5865
Chris Towler <ctowler.518@gmail.com>
2016-02-15 14:10:01 -0500

    Create Board#move

--- board.rb
+++ board.rb
@@ -1,4 +1,8 @@
+require_relative 'piece.rb'
+
 class Board
+  # attr_reader :grid
+
   def initialize
     @grid = Array.new(8) { Array.new(8) }
     setup_pieces
@@ -14,6 +18,32 @@ class Board
     @grid[row][col] = value
   end
 
+  def move(start_pos, end_pos)
+    if self[start_pos].nil?
+      raise ChessError.new("No piece at start location")
+    elsif !valid_move?(start_pos, end_pos)
+      raise ChessError.new("Piece selected can't move there")
+    end
+    self[end_pos] = self[start_pos]
+    self[start_pos] = nil
+  end
+
   def setup_pieces
   end
+
+  def valid_move?(start_pos, end_pos)
+    true
+  end
+end
+
+class ChessError < StandardError
+end
+
+if __FILE__ == $0
+  board = Board.new
+  piece = Piece.new
+  board[[0, 0]] = piece
+  board.move([0, 0], [0, 1])
+  p board[[0, 1]]
+  board.move([0, 0], [0, 1])
 end
